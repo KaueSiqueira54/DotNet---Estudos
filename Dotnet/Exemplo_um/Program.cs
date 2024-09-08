bbe95542a22cb6b1cdb62bc9259a9d9b6b7e5a48
KaueSiqueira <kaue54150@gmail.com>
2024-09-08 18:24:23 -0300

    add

--- Dotnet/Exemplo_um/Program.cs
+++ Dotnet/Exemplo_um/Program.cs
@@ -1,2 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("First line in C#...");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
